7fff30cb366f23d81ecfde239947d827aa2ddf03
Carlos Moreno <omorenodovale@gmail.com>
2021-04-03 11:37:01 -0300

    feat(doc): Add API documentation

--- vough_backend/vough_backend/urls.py
+++ vough_backend/vough_backend/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("api.routes"), name="api"),
+    path('doc/', TemplateView.as_view(
+            template_name='api/swagger-ui.html',
+            extra_context={'schema_url': 'openapi-schema'}
+        ), name='swagger-ui'),
 ]
